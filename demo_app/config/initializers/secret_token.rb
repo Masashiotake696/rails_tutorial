a4845c3f6a76804dcb8c0571fed0460745cea9e0
masashi.otake <masashi.otake@tabian.co>
2020-09-09 01:17:28 +0900

    demo_appの追加

---
+++ demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = 'f24149623a852d7a0e371afad6d3d37900b418ee795afe538d189c2fd725906cbdb73762e4657dc5f26e396ea97af438738ea38acef92bad019a0390929d3c55'
