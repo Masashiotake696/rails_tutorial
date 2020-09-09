b0e24dcd4040193efa1a1613d38d37bceed16985
masashi.otake <masashi.otake@tabian.co>
2020-09-09 21:44:48 +0900

    homeページとhelpページの作成

---
+++ sample_app/app/controllers/static_pages_controller.rb
@@ -0,0 +1,9 @@
+class StaticPagesController < ApplicationController
+  # ビューを指定しない場合は対応するビューが出力される(home.html.erb)
+  def home
+  end
+
+  # ビューを指定しない場合は対応するビューが出力される(help.html.erb)
+  def help
+  end
+end
